4e4735cb8dc3ff6c9b4258c31b60eba02e2520de
Ian Cardinoza <ijcardin@ucalgary.ca>
2019-07-11 18:13:43 -0600

    finished todo list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import todoData from './Components/todoData'
 import TodoItem from './Components/Todoitem'
+import { tsConstructorType } from '@babel/types';
 
 
 // function App() {
@@ -16,8 +17,31 @@ import TodoItem from './Components/Todoitem'
 
 class App extends React.Component {
   // you can add your own method here and then call it inside render
+  constructor() {
+    super() // This line is always needed if you have a constructor
+    this.state = {
+      todos: todoData
+    } // state will always be an object
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedState = prevState.todos.map(todo => {
+        if(todo.id === id){
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      return {
+        todos: updatedState
+      }
+    })  
+  }
+
   render() {
-    const todoItems = todoData.map(item => <TodoItem key={item.id} item={item} />)
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} 
+      handleChange={this.handleChange} />)
     return (
       <div className="todo-list">
         {todoItems}
@@ -25,5 +49,29 @@ class App extends React.Component {
     );
   }
 }
+
+// class App extends React.Component {
+//   constructor() {
+//     super()
+//     this.handleClick = this.handleClick.bind(this);
+//     this.state = {
+//       loggedIn: true
+//     }
+
+//   }
+
+//   handleClick() {
+//    this.state.loggedIn ? this.setState({loggedIn : false}) : this.setState({loggedIn : true})
+//   }
+
+//   render(){
+//     return(
+//       <div>
+//         <h1>{this.state.loggedIn ? "Logged in!" : "Logged out!"}</h1>
+//         <button onClick={this.handleClick}>{this.state.loggedIn ? "log out" : "log in"}</button>
+//       </div>
+//     )
+//   }
+// }
 
 export default App;
--- src/Components/Todoitem.js
+++ src/Components/Todoitem.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
 function TodoItem(props) {
+
+    const completedStyle = {
+        fontStyle: "italic",
+        color: "#cdcdcd",
+        textDecoration: "line-through"
+    }
+
     return (
     <div className="todo-item">
-        <input id='checkboxItem' type="checkbox" checked={props.item.completed} />
-        <label for='checkboxItem'>{props.item.text}</label>
+        <input id='checkboxItem' type="checkbox" checked={props.item.completed}
+        onChange= {() => props.handleChange(props.item.id)} />
+        <label style={props.item.completed? completedStyle : null} for='checkboxItem'>{props.item.text}</label>
+  
     </div>
     )
 }
